42e242b9f5449a1d98ab734c3067582bb9e93e3c
Menace <77221436+ManishNath001@users.noreply.github.com>
2025-10-08 19:41:36 +0530

    Navbar
    Navbar adjusted

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,13 +8,13 @@ const Navbar = () => {
             <div className='flex justify-left items-center'>
                 <a href="/"><img src={Azad} alt='Logo'/></a>
             </div>
-            <div className='pl-65 pt-3 inline-flex gap-20 text-xl justify-center items-center'>
+            <div className='pl-65 pt-1 inline-flex gap-20 text-xl justify-center items-center'>
                 <div className='text-white'><a href=''>About</a></div>
                 <div className='text-white'>Azad<a href=''/></div>
                 <div className='text-white'>Contact Us</div>
                 <div className='text-white'>News</div>
             </div>
-            <div className='pl-70 pt-2'>
+            <div className='pl-70 pt-1'>
                 <button className='w-36 h-10 text-white border border-red-600'>Book Launch</button>
             </div>
         </div>
