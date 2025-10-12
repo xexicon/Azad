e6b6be0d62101af3c45cfb39b1638b33e63aaac2
xexicon <xexicon001@gmail.com>
2025-10-12 23:23:15 +0530

    Frontend Links Insertion

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 // cors
 const allowedOrigins = [
-  'http://localhost:5173',
+  'http://localhost:5173', 'https://azad-frontend.vercel.app', 'https://azad.ocullospace.com'
 ];
 app.use(cors({
   origin: function (origin, callback) {
